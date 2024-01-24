f2f9c39bcfd75d590f0ec6c26e85c2b09b96cb59
Francesco <francesco.pasquinoni@hotmail.it>
2024-01-24 11:45:38 +0100

    data imported, boilerplate set

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("Vue OK", Vue);
+
+const {createApp} = Vue;
+const app = createApp ({
+    name: "Boolzapp",
+    data () {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+})
+
+app.mount("#root");
