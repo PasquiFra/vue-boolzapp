094116be94aa553e9104ac153a3b265b17f2ae5b
Francesco <francesco.pasquinoni@hotmail.it>
2024-01-25 10:40:52 +0100

    MILESTONE 1

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 const data = {
     user: {
-      name: 'Nome Utente',
+      name: 'Walter',
       avatar: '_io'
     },
     contacts: [
--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 console.log("Vue OK", Vue);
 
+const messages = contacts.messages;
+
 const {createApp} = Vue;
 const app = createApp ({
     name: "Boolzapp",
     data () {
         return {
-
+            ...data,
         }
     },
     computed: {
